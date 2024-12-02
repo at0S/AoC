26ee473d9c82a82e7459323d8ce0be34d03f86c0
Taras Yermolenko <t.yermolenko@gmail.com>
2024-12-02 12:18:50 +1100

    [AoC] chore: formatting and typing

--- 2024/day-1/day-1.py
+++ 2024/day-1/day-1.py
@@ -1,20 +1,20 @@
-def read_input() -> str:
-    data = ''
-    with open('input') as f:
+def read_input() -> list[str]:
+    data = ""
+    with open("input") as f:
         data = f.readlines()
     return data
 
 def process(line: str) -> tuple[int, int]:
-    left, right = line.split()
-    return int(left), int(right)
+  left, right = line.split()
+  return int(left), int(right)
 
 def calculate(left: list, right: list) -> int:
-    final = 0
-    left = sorted(left)
-    right = sorted(right)
-    for i in range(len(left)):
-        final = final + abs(right[i] - left[i])
-    return final
+  final = 0
+  left = sorted(left)
+  right = sorted(right)
+  for i in range(len(left)):
+      final = final + abs(right[i] - left[i])
+  return final
 
 def similarity(number: int, right: list) -> int:
     score = 0
@@ -23,11 +23,13 @@ def similarity(number: int, right: list) -> int:
             score = score + 1
     return number * score
 
+
 def similarity_score(left: list, right: list) -> int:
     similarity_score = 0
     for i in range(len(left)):
         similarity_score = similarity_score + similarity(left[i], right)
     return similarity_score
+
 
 def main():
     left, right = [], []
@@ -36,10 +38,9 @@ def main():
         l, r = process(line)
         left.append(l)
         right.append(r)
-
     print(calculate(left, right))
     print(similarity_score(left, right))
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
