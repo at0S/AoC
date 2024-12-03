d02e6fa4a38b2edc45d54bdc42047c7a887bf727
Taras Yermolenko <t.yermolenko@gmail.com>
2024-12-03 11:27:39 +1100

    [AoC] new: now we'll deal with lists of integers, not strings!

--- 2024/day-2/day_2.py
+++ 2024/day-2/day_2.py
@@ -5,41 +5,40 @@ def read_input() -> list[str]:
     return data
 
 
-def validate_asc(line: str) -> bool:
-    l = list(map(int, line.split()))
+def validate_asc(l: list[int]) -> bool:
     s = sorted(l)
     return l == s
 
 
-def validate_desc(line: str) -> bool:
-    l = list(map(int, line.split()))
+def validate_desc(l: list[int]) -> bool:
     s = sorted(l, reverse=True)
     return l == s
 
 
-def validate_diff(line: str, diff: int) -> bool:
-    l = list(map(int, line.split()))
+def validate_diff(l: list[int], diff: int) -> bool:
     for i in range(len(l) - 1):
         if abs(l[i] - l[i + 1]) > diff:
             return False
     return True
 
 
-def validate_set(line: str) -> bool:
-    l = list(map(int, line.split()))
+def validate_set(l: list[int]) -> bool:
     s = set(l)
     return len(l) == len(s)
 
 
 def main():
     safe = 0
+    diff = 3
     lines = read_input()
     for line in lines:
-        if validate_asc(line) or validate_desc(line):
-            if validate_set(line) and validate_diff(line, 3):
+        l = list(map(int, line.split()))
+        if validate_asc(l) or validate_desc(l):
+            if validate_set(l) and validate_diff(l, diff):
                 safe += 1
         else:
-            print(f"{line} is not safe")
+            print(f"{l} is not safe, need a further validation!")
+
     print(safe)
 
 
--- 2024/day-2/tests/test_day_2.py
+++ 2024/day-2/tests/test_day_2.py
@@ -4,34 +4,34 @@ sys.path.insert(0, "/Users/at0S/github.com/AoC/2024/day-2")
 from  day_2 import validate_diff, validate_asc, validate_desc, validate_set
 
 def test_diff_simple_1():
-    assert validate_diff("1 2 3 4 5", 3) == True
+    assert validate_diff([1, 2, 3, 4, 5], 3) == True
 
 def test_diff_simple_2():
-    assert validate_diff("10 20 30 40 50", 3) == False
+    assert validate_diff([10, 20, 30, 40, 50], 3) == False
 
 def test_diff_simple_3():
-    assert validate_diff("20 19 16 15 12 9", 3) == True
+    assert validate_diff([20, 19, 16, 15, 12, 9], 3) == True
 
 def test_validate_asc_1():
-    assert validate_asc("1 2 3 4 5") == True
+    assert validate_asc([1, 2, 3, 4, 5]) == True
 
 def test_validate_asc_2():
-    assert validate_asc("5 4 3 2 1") == False
+    assert validate_asc([5, 4, 3, 2, 1]) == False
 
 def test_validate_asc_3():
-    assert validate_asc("20 19 16 15 12 9") == False
+    assert validate_asc([20, 19, 16, 15, 12, 9]) == False
 
 def test_validate_desc_1():
-    assert validate_desc("5 4 3 2 1") == True
+    assert validate_desc([5, 4, 3, 2, 1]) == True
 
 def test_validate_desc_2():
-    assert validate_desc("1 2 3 4 5") == False
+    assert validate_desc([1, 2, 3, 4, 5]) == False
 
 def test_validate_desc_3():
-    assert validate_desc("20 19 16 15 12 9") == True
+    assert validate_desc([20, 19, 16, 15, 12, 9]) == True
 
 def test_validate_set_1():
-    assert validate_set("1 2 3 4 5") == True
+    assert validate_set([1, 2, 3, 4, 5]) == True
 
 def test_validate_set_2():
-    assert validate_set("1 2 3 4 4") == False
+    assert validate_set([1, 2, 3, 4, 4]) == False
