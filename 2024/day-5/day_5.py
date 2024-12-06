f65a803efd7643c8e0cbdbb79db3507f81bf3b80
Taras Yermolenko <t.yermolenko@gmail.com>
2024-12-06 14:33:34 +1100

    [AoC] new: a bit of refactoring and solved day-5 part 2

--- 2024/day-5/day_5.py
+++ 2024/day-5/day_5.py
@@ -8,13 +8,31 @@ def get_list_midsection(data: list[str]) -> int:
     m = len(data) // 2 
     return int(data[int(m)])
 
-def check_order(left: str, right: str, order: dict) -> bool:
-    if left in order.keys():
-        if right not in order[left]:
+def correctly_ordered(key: str, value: str, order: dict) -> bool:
+    if key in order.keys():
+        if value not in order[key]:
             return False
     else:
-        return not check_order(right, left, order)
+        return not correctly_ordered(value, key, order)
     return True
+
+def filter_print_order(sequence: list[str], order: dict):
+    for i in range(len(sequence) - 1):
+        left = sequence[i]
+        right = sequence[i+1]
+        if not correctly_ordered(left, right, order):
+            return None
+    return sequence
+
+def fix_print_order(sequence: list[str], order: dict):
+    for i in range(len(sequence) - 1):
+        left = sequence[i]
+        right = sequence[i+1]
+        if not correctly_ordered(left, right, order):
+            sequence[i] = right
+            sequence[i+1] = left
+            sequence = fix_print_order(sequence, order)
+    return sequence
 
 def main():
     page_order = {}
@@ -32,16 +50,18 @@ def main():
             if len(line.strip()) > 0:
                 print_sequence.append(line.strip())
 
-    middle_numbers = 0
+    original_middle = 0
+    fixed_middle = 0
     for ps in print_sequence:
         ps = ps.split(",")
-        for i in range(len(ps) - 1):
-            if not check_order(ps[i], ps[i+1], page_order):
-                break
-        else:
-            middle_numbers += get_list_midsection(ps)
-                    
-    print(f"Middle Numbers Sum: {middle_numbers}")
+        original = filter_print_order(ps, page_order)
+        fixed = fix_print_order(ps, page_order)
+        if original is not None:
+            original_middle += get_list_midsection(original)
+        fixed_middle += get_list_midsection(fixed)
+    print(f"Page Order: {page_order} \nPrint Sequence: {print_sequence}")                    
+    print(f"Original Middle Numbers Sum: {original_middle}")
+    print(f"Fixed Middle Numbers Sum: {fixed_middle - original_middle}")
 
 if __name__ == "__main__":
     main()
--- 2024/day-5/tests/test_day_5.py
+++ 2024/day-5/tests/test_day_5.py
@@ -1,10 +1,18 @@
 import sys
 sys.path.insert(0, "/Users/at0S/github.com/AoC/2024/day-5")
 
-from  day_5 import get_list_midsection
+from  day_5 import get_list_midsection, process_print_order
 
 def test_get_list_midsection_1():
     assert get_list_midsection(["1", "2", "3", "4", "5"]) == 3
 
 def test_get_list_midsection_2():
     assert get_list_midsection(["1", "2", "3", "4", "5", "6","7","8","9","10","11"]) == 6
+
+def test_get_list_midsection_3():
+    assert get_list_midsection(['97', '75', '47', '29', '13']) == 47
+
+def test_process_print_order():
+    order={'47': ['53', '13', '61', '29'], '97': ['13', '61', '47', '29', '53', '75'], '75': ['29', '53', '47', '61', '13'], '61': ['13', '53', '29'], '29': ['13'], '53': ['29', '13']}
+    sequence = ['97','13','75','29','47']
+    assert process_print_order(sequence, order) == ['97', '75', '47', '29', '13']
