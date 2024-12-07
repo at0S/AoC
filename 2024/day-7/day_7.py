f1909fb74069005843b1de568d699bfb1129dd8b
Taras Yermolenko <t.yermolenko@gmail.com>
2024-12-07 17:06:24 +1100

    [AoC] infra: get day 7 ready

---
+++ 2024/day-7/day_7.py
@@ -0,0 +1,11 @@
+def read_input() -> list[str]:
+    data = ""
+    with open("input") as f:
+        data = f.readlines()
+    return data
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/day-7/tests/test_day_7.py
@@ -0,0 +1,5 @@
+import sys
+
+sys.path.insert(0, "/Users/at0S/github.com/AoC/2024/day-7")
+
+from day_7 import *
